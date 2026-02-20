1d4420f3405de3a3801235d18cb012ad164fa210
AarishSLU <anaumaan@slu.edu>
2026-02-20 00:06:08 -0600

    Update hello_write.c

--- studio-1-hello-world/hello_write.c
+++ studio-1-hello-world/hello_write.c
@@ -1,1 +1,10 @@
-//Replace the contents of this file with your hello_write.c
+// Aarish
+// 2026/02/19
+
+#include <unistd.h>
+
+int main(int argc, char* argv[]){
+    const char msg[] = "Hello, world!\n";
+    write(STDOUT_FILENO, msg, sizeof(msg) - 1);
+    return 0;
+}
